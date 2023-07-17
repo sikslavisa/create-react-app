c437c4c132fdea236b5efeac647eaf9a2175c744
sikslavisa <133794415+sikslavisa@users.noreply.github.com>
2023-07-17 19:30:12 +0200

    Create App.js
    1. Import the React library
    2. Render the Weather component
    3. Export the App component as the default export

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react'; // Import the React library
+import Weather from './Weather'; // Import the Weather component from the './Weather' file
+
+function App() {
+  return (
+    <div className="App"> {/* Root container for the app */}
+    {/* Render the Weather component */}
+        
+      <Weather />
+    </div>
+  );
+}
+
+export default App; // Export the App component as the default export
